b2cf41e214c86c38bab54642ce1b7a3d14b2034b
Hao079231 <haotrinh2003.com@gmail.com>
2025-10-14 21:11:51 +0700

    Trung Hào: Sửa code API trong controller và router

--- routes/Address.js
+++ routes/Address.js
@@ -1,11 +1,12 @@
-const express=require('express')
-const addressController=require("../controllers/Address")
-const router=express.Router()
+const express = require('express')
+const addressController = require("../controllers/Address")
+const { verifyToken } = require("../middleware/VerifyToken")
+const router = express.Router()
 
 router
-    .post("/",addressController.create)
-    .get("/user/:id",addressController.getByUserId)
-    .patch('/:id',addressController.updateById)
-    .delete('/:id',addressController.deleteById)
+    .post("/", verifyToken, addressController.create)
+    .get("/user", verifyToken, addressController.getByUserId)
+    .patch('/:id', verifyToken, addressController.updateById)
+    .delete('/:id', verifyToken, addressController.deleteById)
 
-module.exports=router
+module.exports = router
